59c488a9941d00263fea4d8d1b174d2693e9938e
Suchita Manohare <38423185+suchitadudhmal358@users.noreply.github.com>
2023-01-17 12:21:11 +0530

    State counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  // var count = 0;
+  // const resetCount = () => (count = 0);
+  // const increaseCount = () => (count = count + 1);
+  // const decCount = () => (count -= 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Counter app using State/ hooks</h1>
       </header>
+      <h2>Current value of count is {count}</h2>
+      {/* <button onClick={resetCount}>Reset Counter</button>
+      <button onClick={increaseCount}>Increase Counter</button>
+      <button onClick={decCount}>Decrase Counter</button> */}
+
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => (count >= 10 ? "" : setCount(count + 1))}>
+        Increase Counter
+      </button>
+      <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
+        Decrease Counter
+      </button>
     </div>
   );
-}
+};
 
 export default App;
